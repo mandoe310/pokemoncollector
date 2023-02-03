fce3854556fc20b9b2e94f02b96dfeb62e9fd380
mandoe310 <mandoe1@yahoo.com>
2023-02-03 15:21:48 -0800

    adds part 1

--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
 
-# Create your views here.
+pokemons = [
+    {'name': 'Charmander', 'type': 'fire', 'description': 'fire breathing amphibian', 'special': 'ember', 'hp': '50'},
+    {'name': 'Squirtle', 'type': 'water', 'description': 'water pumping turtle', 'special': 'bubble', 'hp': '40'},
+    {'name': 'Bulbasaur', 'type': 'grass', 'description': 'vine whipping frog', 'special': 'leech seed', 'hp': '40'},
+    {'name': 'Pikachu', 'type': 'electric', 'description': 'electrifying yellow squirrel', 'special': 'thunder jolt', 'hp': '40'},
+]
+
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+    return render(request, 'about.html')
+
+def pokemons_index(request):
+    return render(request, 'pokemons/index.html', {
+        'pokemons': pokemons
+    })
