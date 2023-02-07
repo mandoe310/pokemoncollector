accea96d9eb00e92079ad0a8cc2ea9c7b14cb95b
mandoe310 <mandoe1@yahoo.com>
2023-02-06 17:04:02 -0800

    adds part 2 and 3

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Pokemon(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    special = models.TextField(max_length=100)
+    hp = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'pokemon_id': self.id})
--- main_app/views.py
+++ main_app/views.py
@@ -1,11 +1,7 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Pokemon
 
-pokemons = [
-    {'name': 'Charmander', 'type': 'fire', 'description': 'fire breathing amphibian', 'special': 'ember', 'hp': '50'},
-    {'name': 'Squirtle', 'type': 'water', 'description': 'water pumping turtle', 'special': 'bubble', 'hp': '40'},
-    {'name': 'Bulbasaur', 'type': 'grass', 'description': 'vine whipping frog', 'special': 'leech seed', 'hp': '40'},
-    {'name': 'Pikachu', 'type': 'electric', 'description': 'electrifying yellow squirrel', 'special': 'thunder jolt', 'hp': '40'},
-]
 
 def home(request):
   return render(request, 'home.html')
@@ -14,6 +10,24 @@ def about(request):
     return render(request, 'about.html')
 
 def pokemons_index(request):
+    pokemons = Pokemon.objects.all()
     return render(request, 'pokemons/index.html', {
         'pokemons': pokemons
     })
+
+def pokemons_detail(request, pokemon_id):
+    pokemon = Pokemon.objects.get(id=pokemon_id)
+    return render(request, 'pokemons/detail.html', { 'pokemon': pokemon })
+
+class PokemonCreate(CreateView):
+    model = Pokemon
+    fields = '__all__'
+    # success_url = '/pokemons'
+
+class PokemonUpdate(UpdateView):
+  model = Pokemon
+  fields = ['type', 'description', 'special', 'hp']
+
+class PokemonDelete(DeleteView):
+  model = Pokemon
+  success_url = '/pokemons'
